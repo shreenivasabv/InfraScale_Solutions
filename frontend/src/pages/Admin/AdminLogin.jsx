5e291c316bd2b36b66ef8981d5a39e4368a8826d
shreeenivasabv <shreenivasbv104@gmailcom>
2026-03-06 16:43:13 +0530

    url fixing

--- frontend/src/pages/Admin/AdminLogin.jsx
+++ frontend/src/pages/Admin/AdminLogin.jsx
@@ -5,6 +5,9 @@ import "./Admin.css";
 
 
 const API_BASE = import.meta.env.VITE_API_URL || "http://localhost:5000";
+console.log("API URL:", API_BASE);
+
+
 
 function AdminLogin() {
   const [email, setEmail] = useState("");
